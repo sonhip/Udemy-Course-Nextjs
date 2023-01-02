acad692f9f144fa50a47b32c126596b5bfa2430f
SonTV <son.tv@shb.com.vn>
2023-01-02 22:25:33 +0700

    update next/link

--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
 import DateIcon from "../icons/date-icon";
@@ -25,7 +26,7 @@ function EventItem(props: EventItemProps) {
   const exploreLink = `events/${id}`;
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={250} height={300} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
